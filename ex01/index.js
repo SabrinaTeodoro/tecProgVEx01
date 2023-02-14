fa984cf49ecd3583733484a19cb183b85ece3e28
SabrinaTeodoro <102275074+SabrinaTeodoro@users.noreply.github.com>
2023-02-14 20:26:51 -0300

    Upload

---
+++ ex01/index.js
@@ -0,0 +1,11 @@
+/* mostrar tutorial */
+//ao clicar deve abrir um modal com btn "proximo"
+//mostrando o passo-a-passo + 1 checkbox -> mostrar novamente ao nao -> salvar no local storage para proxima vez que for abrir a pag
+
+/* inserindo texto */
+const btnSalvar = document.getElementById("btnSalvar");
+btnSalvar.addEventListener("click", addTarefa);
+
+function addTarefa(){
+    console.log('NYOHO');
+}
